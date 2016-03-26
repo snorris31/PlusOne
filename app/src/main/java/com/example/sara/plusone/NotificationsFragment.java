5c78dcfc2d47cb266df09b38e79b08877359e27a
Zack Littke-Smith <zdizzler@yahoo.com>
2016-03-26 17:32:03 -0400

    notifications!

--- app/src/main/java/com/example/sara/plusone/NotificationsFragment.java
+++ app/src/main/java/com/example/sara/plusone/NotificationsFragment.java
@@ -7,8 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.sara.plusone.adapters.NotificationAdapter;
+import com.example.sara.plusone.objects.Notification;
+
+import java.util.ArrayList;
 
 public class NotificationsFragment extends Fragment {
+
+    ListView listView;
+    NotificationAdapter adapter;
 
     public NotificationsFragment() {
     }
@@ -17,6 +26,14 @@ public class NotificationsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        listView = (ListView)view.findViewById(R.id.notification_list);
+        ArrayList<Notification> notifications = new ArrayList<>();
+        notifications.add(new Notification("-1","-1","-1","-1","this is the description of a notification", false));
+        notifications.add(new Notification("-1","-1","-1","-1","this is the description of a notification. but this ones a much longer description. good for you. aosgfiabd  ihbdus fiusybd fusydfbsd fbsudyfbsjdfbsjd uysvd vftsvd fusydbsbdfsbdufysdf sdfsdf", false));
+        notifications.add(new Notification("-1","-1","-1","-1","this is the description of a notification", true));
+        adapter = new NotificationAdapter(getActivity(), R.layout.notification_segment, notifications);
+        listView.setAdapter(adapter);
 
         return view;
     }
