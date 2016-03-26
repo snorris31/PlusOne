f2c147cee59808a992cc86e1c1c6b33b3c96183a
SARA NORRIS <saramnorris@gatech.edu>
2016-03-26 00:02:35 -0400

    added tab bar

--- app/src/main/java/com/example/sara/plusone/MainActivity.java
+++ app/src/main/java/com/example/sara/plusone/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.sara.plusone;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager mPager;
+    ScreenSlider mPagerAdapter;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        mPager = (ViewPager) findViewById(R.id.pager);
+        tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        mPagerAdapter = new ScreenSlider(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        tabLayout.setupWithViewPager(mPager);
     }
 
     @Override
@@ -49,4 +65,49 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class ScreenSlider extends FragmentStatePagerAdapter {
+        public ScreenSlider(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new HomeFragment();
+                case 1:
+                    return new EventsFragment();
+                case 2:
+                    return new MessagesFragment();
+                case 3:
+                    return new NotificationsFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Home";
+                case 1:
+                    return "Events";
+                case 2:
+                    return "Messages";
+                case 3:
+                    return "Notifications";
+            }
+            return null;
+        }
+    }
 }
+
+
+
+
