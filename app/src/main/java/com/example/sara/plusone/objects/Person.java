b41675ee92a1d111be9fb66b5ebe798b8cdafbb0
Zack Littke-Smith <zdizzler@yahoo.com>
2016-03-25 22:03:28 -0400

    Added some enums and objects

---
+++ app/src/main/java/com/example/sara/plusone/objects/Person.java
@@ -0,0 +1,27 @@
+package com.example.sara.plusone.objects;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Zack on 3/25/2016.
+ */
+
+public class Person {
+
+    public String id;
+
+    public String name;
+    public int age;
+    public Drawable picture;
+
+    public Person(String name, int age, Drawable picture) {
+        this("-1", name, age, picture);
+    }
+
+    public Person(String id, String name, int age, Drawable picture) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.picture = picture;
+    }
+}
